c1d888ff9c16e1625d4016ea151d59552d82eee8
unknown <U65983@F152LTRV.ustr.com>
2022-03-06 02:35:11 +0530

    final changes

--- util.py
+++ util.py
@@ -2,16 +2,20 @@ from configparser import ConfigParser
 
 def parse_xml_data_types(sec_name):
     config_parser = ConfigParser()
+    config_parser.optionxform=str
     config_parser.read("xml.config")
     var_type_dict = {}
     final_dtype={}
     for section_name in config_parser.sections():
         if section_name == sec_name:
             for index in config_parser.items(section_name):
-                var_type_dict[index[0]] = index[1].split(",")
-                for i in var_type_dict[index[0]]:
-                    if index[0]=="string":
-                        final_dtype[i]=str
-                    else:
-                        final_dtype[i]=float
+                if sec_name=="dtypes":
+                    var_type_dict[index[0]] = index[1].split(",")
+                    for i in var_type_dict[index[0]]:
+                        if index[0]=="string":
+                            final_dtype[i]=str
+                        else:
+                            final_dtype[i]=float
+                else:
+                    final_dtype[index[0]]=index[1]
     return final_dtype
